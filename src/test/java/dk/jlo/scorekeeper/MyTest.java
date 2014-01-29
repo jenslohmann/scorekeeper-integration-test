1cd35a00a0070c64fde7b1f722a701bc53e3213a
jenslohmann <jens@famlohmann.dk>
2014-01-29 15:37:54 +0100

    Got arquillian persistence extension running.

--- src/test/java/dk/jlo/scorekeeper/MyTest.java
+++ src/test/java/dk/jlo/scorekeeper/MyTest.java
@@ -1,7 +1,13 @@
 package dk.jlo.scorekeeper;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.persistence.*;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
@@ -14,27 +20,56 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+//@CreateSchema({"schema-creation.sql"})
 @RunWith(Arquillian.class)
+@PersistenceTest
+@DataSource("java:/ds/postgresDS")
+@UsingDataSet("tournaments.yml")
 public class MyTest {
 
+    @Deployment(testable = true, name = "ArqPersistencePluginHack", order = 0)
+    @OverProtocol("Servlet 3.0") // Avoid JBAS016000
+    public static EnterpriseArchive hackForPlugin() {
+        return ShrinkWrap.create(EnterpriseArchive.class, "test.ear")
+                .addAsLibrary(ShrinkWrap.create(JavaArchive.class, "test.jar")
+                        .addClass(MyTest.class)
+                                // Avoid BeanManager not found (enables CDI)
+                        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                        .addAsResource("META-INF/persistence.xml"))
+                .addAsLibrary(Maven.resolver().resolve("dk.jlo.scorekeeper:model:jar:1.0.0-SNAPSHOT").withoutTransitivity()
+                .asSingleFile());
+    }
+
     @Deployment(testable = false, name = "scorekeeper", order = 1)
+    @OverProtocol("Servlet 3.0") // Avoid JBAS016000
     public static EnterpriseArchive importEar() {
         return Maven.resolver() // get resolver instance
                 .resolve("dk.jlo.scorekeeper:scorekeeper-ear:ear:1.0.0-SNAPSHOT") // get EAR from Maven repository - note you might want to set your own Maven repository first via document above
-                .withoutTransitivity() // you don't need any ear's transitive deps
+                .withoutTransitivity() // Don't need any ear's transitive dependencies
                 .asSingle(EnterpriseArchive.class); // wrap the result as single object of type EnterpriseArchive
     }
 
-    @Deployment(order = 1000)
-    public static JavaArchive deployTestStuff() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    @Test
+    @InSequence(1)
+//    @UsingDataSet("tournaments.yml")
+    @OperateOnDeployment("ArqPersistencePluginHack")
+    @Cleanup(phase = TestExecutionPhase.NONE)
+    public void hackThePlugin() {
+        System.out.println("PLUGIN HACK!");
+        assertThat(1, is(1));
     }
 
     @Test
-    public void test() throws IOException {
-        // FIXME Use port no etc. from Arquillian
-        URL url = new URL("http://localhost:8180/ejb-1.0.0-SNAPSHOT/MatchWS/MatchWS");
+    @InSequence(2)
+    @OperateOnDeployment("scorekeeper")
+    @RunAsClient
+    public void test(@ArquillianResource URL testUrl) throws IOException {
+        System.out.println("URL:" + testUrl);
+        URL url = new URL(testUrl.getProtocol() + "://" + testUrl.getHost() + ":" + testUrl.getPort()
+                + "/ejb-1.0.0-SNAPSHOT/MatchWS/MatchWS");
         HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         String xmlInput = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" " +
---
+++ src/test/java/dk/jlo/scorekeeper/UsingPersistenceTest.java
@@ -0,0 +1,62 @@
+package dk.jlo.scorekeeper;
+
+import dk.jlo.scorekeeper.model.Tournament;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.container.test.api.OverProtocol;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.junit.InSequence;
+import org.jboss.arquillian.persistence.*;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Demonstrates basic use of the Persistence Extension stuff.
+ */
+//@CreateSchema({"schema-creation.sql"})
+@RunWith(Arquillian.class)
+@PersistenceTest
+@DataSource("java:/ds/postgresDS")
+@UsingDataSet("tournaments.yml")
+public class UsingPersistenceTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Deployment(testable = true, name = "ArqPersistencePluginHack", order = 0)
+    @OverProtocol("Servlet 3.0") // Avoid JBAS016000
+    public static EnterpriseArchive hackForPlugin() {
+        return ShrinkWrap.create(EnterpriseArchive.class, "PersistenceTest.ear")
+                .addAsLibrary(ShrinkWrap.create(JavaArchive.class, "test.jar")
+                        .addClass(UsingPersistenceTest.class)
+                                // Avoid BeanManager not found (enables CDI)
+                        .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
+                        .addAsResource("META-INF/persistence.xml"))
+                .addAsLibrary(Maven.resolver().resolve("dk.jlo.scorekeeper:model:jar:1.0.0-SNAPSHOT").withoutTransitivity()
+                        .asSingleFile());
+    }
+
+    @Test
+    @InSequence(1)
+    @UsingDataSet("tournaments.yml")
+    @OperateOnDeployment("ArqPersistencePluginHack")
+    public void hackThePlugin() {
+        System.out.println("PLUGIN HACK!");
+        assertThat(1, is(1));
+
+        List<Tournament> result = em.createQuery("select t from Tournament t").getResultList();
+    }
+}
